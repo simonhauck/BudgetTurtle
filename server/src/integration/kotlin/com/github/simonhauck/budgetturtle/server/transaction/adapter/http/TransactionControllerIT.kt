888a267bbc34859bd6a9e42400b7f3edcfbca87d
Simon Hauck <simon.hauck@gmx.de>
2023-03-11 00:51:46 +0100

    #18 fix backend test

--- server/src/integration/kotlin/com/github/simonhauck/budgetturtle/server/transaction/adapter/http/TransactionControllerIT.kt
+++ server/src/integration/kotlin/com/github/simonhauck/budgetturtle/server/transaction/adapter/http/TransactionControllerIT.kt
@@ -4,12 +4,12 @@ import com.github.simonhauck.budgetturtle.server.IntegrationTest
 import org.junit.jupiter.api.Test
 
 class TransactionControllerIT : IntegrationTest() {
-    private val user = "SomeFakeUser"
+    private val someExistingUser = "SomeFakeUser"
 
     @Test
     fun `get all returns an empty list of transactions for a new user`() =
         testDriver(this) {
-            val actual = get("/api/transactions/user/$user/last")
+            val actual = get("/api/transactions/user/someUnknownUser/last")
 
             actual.body.assertJsonEquals(readAndNormalize("getTransaction_new_user_expected.json"))
         }
@@ -17,9 +17,9 @@ class TransactionControllerIT : IntegrationTest() {
     @Test
     fun `get all returns an the list of stored transactions for the existing user`() =
         testDriver(this) {
-            createTransactionForUser(user)
+            createTransactionForUser(someExistingUser)
 
-            val actual = get("/api/transactions/user/$user/last")
+            val actual = get("/api/transactions/user/$someExistingUser/last")
 
             actual.body.assertJsonEquals(
                 readAndNormalize("getTransaction_details_expected.json"),
